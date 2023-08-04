1d64019d5cd9cc96581fe3c4bbb8b8058391e922
ychwang <yc1128@naver.com>
2023-08-04 15:04:11 +0900

    day5homework

---
+++ test.c/day5.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+    int rand1,rand2,x=10,y=0;
+    int count =0,idx=0;
+    int password=1,num;
+    srand(time(NULL));
+    while(1)
+    {
+        
+        rand1 = rand()%x+y;
+        rand2 = rand()%x+y;
+        num = rand1* rand2;
+        printf("########### %d번째 비밀번호##########\n",idx);
+        printf("%d x %d?\n",rand1,rand2);
+        printf("#############################################\n");
+        printf("비밀번호를 입력하세요.(종료를 원하면 -1 입력)>>");
+        scanf("%d",&password);
+
+        if(password == num) 
+        {   
+            printf(">>Good! 정답입니다.\n");
+            count++;
+            x+=10;
+            y+=10;
+        }
+        else if(password == -1) break;
+        else printf(">> 땡! 틀렸습니다.\n");
+        idx++;
+    }
+    printf("비밀번호 %d개를 맞혔습니다.\n",count);
+    return 0;
+}
